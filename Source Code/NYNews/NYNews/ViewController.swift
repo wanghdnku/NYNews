2e00606aa44c48c2e5b3877030a97f033bc9d0f5
Haidong Wang <wanghd.nku@outlook.com>
2016-07-19 15:10:34 +0800

    创建一个新的工程

---
+++ Source Code/NYNews/NYNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NYNews
+//
+//  Created by Hayden on 16/7/19.
+//  Copyright © 2016年 01ny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
